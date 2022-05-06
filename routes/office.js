b33e09f6ee9cc708b591730dffc159ff5ddafd27
rohanhr3452 <rohanhr3452@gmail.com>
2022-05-06 22:53:53 +0530

    main

--- routes/office.js
+++ routes/office.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const User = require('../models/registeredUsers')
 const router = express.Router()
 
 router.get('/', async (req,res) => {
- res.render("office/index")
- })
+    res.render("office/index")
+})
+
+// Details --------------------------------------------------
+
+router.get('/faculty-details', async (req,res) => {
+    const faculties = await User.find({ role: "teacher" })
+    res.render("office/faculty-details", { faculties : faculties })
+})
+router.get('/student-details', async (req,res) => {
+    const students = await User.find({ role: "student" })
+    res.render("office/student-details", { students : students })
+})
 
  module.exports = router

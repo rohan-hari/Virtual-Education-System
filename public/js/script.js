e29660cbe12a46cf14bf9fc543ca9a134344a737
rohanhr3452 <rohanhr3452@gmail.com>
2022-05-06 20:23:19 +0530

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -10,14 +10,33 @@ $(document).ready(function(){
    });
 });
 
-/* Assignment */
+/* Assignment Modal */
 
 function createAssignment(){
     document.getElementById('create-assignment-model').style.display = "table-row";
 }
 
-/* Notification */
+/* Notification Modal */
 
 function addNotification() {
     document.getElementById('add-notification-model').style.display = "block";
+}
+
+// Multi Select Dropdown
+
+var checkList = document.getElementById('category');
+var items = document.getElementById('items');
+checkList.getElementsByTagName('span')[0].onclick = function(evt) {
+  if (items.classList.contains('visible')) {
+    items.classList.remove('visible');
+    items.style.display = "none";
+  } else {
+    items.classList.add('visible');
+    items.style.display = "block";
+  }
+
+}
+
+items.onblur = function(evt) {
+  items.classList.remove('visible');
 }
